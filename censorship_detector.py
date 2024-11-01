49e38276d254a39ce4bac33b3560ae79f3476a3b
Riccardo Fiorilla <rfiorilla@gmail.com>
2024-11-01 16:11:53 -0400

    final

--- censorship_detector.py
+++ censorship_detector.py
@@ -35,7 +35,7 @@ def comparison():
 	with open("output_good.csv", "r") as f_good, open("output_bad.csv", "r") as f_bad, open("mismatched_resolutions.csv", "w") as f_mism:
 		r_good = csv.reader(f_good)
 		r_bad = csv.reader(f_bad)
-		csv.writer(f_mism).writerow(["Domain", "Good IP Address", "Bad IP Address"])
+		csv.writer(f_mism).writerow(["Domain", "Control IP Address", "Test IP Address"])
 		next(r_good)
 		next(r_bad)
 		cnt = 0
@@ -49,39 +49,51 @@ def comparison():
 def certificate_check(mism_resol):
 	with open("mismatched_resolutions.csv", "r") as f_in, open("certificates.csv", "w") as f_out:
 		r_in = csv.reader(f_in)
-		csv.writer(f_out).writerow(["Domain", "Untrusted Certificate", "Mismatched Name", "Certificate Subject"])
+		csv.writer(f_out).writerow(["Domain", "Untrusted Certification Chain", "Mismatched Name", "Certificate Subject"])
 		next(r_in)
 		cnt = 0
 		mism = 0
 		invalid_mism = 0
 		invalid = 0
+		subprocess.run("rm -rf certificates/*", shell=True, capture_output=True, text=True)
 		for row in r_in:
+			suspicious = 0
 			flag = 0
 			expiration = 0
 			try:
-				result = subprocess.Popen("openssl s_client -showcerts -connect" + " " + row[0] + ":443", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-				stdout, stderr = result.communicate(input=b'Q\n', timeout=3)
+				result = subprocess.Popen("openssl s_client -showcerts -connect" + " " + row[0] + ":443 -CAfile ./mozilla_trusted_ca_list.crt", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+				stdout, stderr = result.communicate(input=b'Q\n', timeout=4)
 			except subprocess.TimeoutExpired:
 				result.kill()
 				expiration = 1
 			if expiration == 0:
-				san = subprocess.run("echo" + " \"" + stdout.decode() + "\" | " + "openssl x509 -noout -text | grep -A 1 \"Subject Alternative Name\"", shell=True, capture_output=True, text=True)
+				san = subprocess.run("echo" + " \"" + stdout.decode() + "\" | " + "openssl x509 -noout -text", shell=True, capture_output=True, text=True)
 				try:
 					subject = stdout.decode().splitlines()[3].split("CN")[-1].lstrip("=")
 				except IndexError:
 					flag = 1
+				#print(san.stdout)
+				#print("-------------------------------------------------------")
 				if flag == 0:
 					if stdout.decode().find("Verification: OK") != -1:
-						if san.stdout.find(row[0]) == -1:
+						if san.stdout.find(row[0]) == -1 and san.stdout.find('.'.join(row[0].split('.')[-2:])) == -1 and subject.find(row[0]) == -1 and subject.find('.'.join(row[0].split('.')[-2:])) == -1:
 							csv.writer(f_out).writerow([row[0], "N", "Y", subject])
 							mism += 1
+							suspicious = 1
+							#print(san)
+							#print("----------------------------------------------------")
 					else:
-						if san.stdout.find(row[0]) == -1:
+						if san.stdout.find(row[0]) == -1 and san.stdout.find('.'.join(row[0].split('.')[-2:])) == -1 and subject.find(row[0]) == -1 and subject.find('.'.join(row[0].split('.')[-2:])) == -1:
 							csv.writer(f_out).writerow([row[0], "Y", "Y", subject])
 							invalid_mism += 1
+							suspicious = 1
 						else:
 							csv.writer(f_out).writerow([row[0], "Y", "N", subject])
 							invalid += 1
+							suspicious = 1
+					if suspicious == 1:
+						with open ("certificates/" + row[0].replace('.', '_') + ".txt", "w") as cert:
+							cert.write("---------- ORIGINAL FORMAT ----------\n" + stdout.decode() + "\n" + "---------- READABLE FORMAT ----------\n" + san.stdout)
 			cnt += 1
 			percentage(cnt / mism_resol * 100)
 	print(f"\tCompleted: 100.00%")
@@ -147,16 +159,16 @@ def curler(certs):
 	print(f"\t{curled} pages retrieved.")
 
 def main():
-	print(f"\tCreating a list of untrusted resolutions...")
-	domains = []
-	input(domains)
-	bad_resolutions(domains)
-	print(f"\tList of untrusted resolutions created -> ./output_bad.csv")
-	print(f"\tComparing good and bad resolutions...")
-	mismatched_resolutions = comparison()
-	print(f"\tList of mismatched resolutions created -> ./mismatched_resolutions.csv")
+	#print(f"\tCreating a list of untrusted resolutions...")
+	#domains = []
+	#input(domains)
+	#bad_resolutions(domains)
+	#print(f"\tList of untrusted resolutions created -> ./output_bad.csv")
+	#print(f"\tComparing good and bad resolutions...")
+	#mismatched_resolutions = comparison()
+	#print(f"\tList of mismatched resolutions created -> ./mismatched_resolutions.csv")
 	print(f"\tAnalyzing the certificates of websites with mismatched resolutions...")
-	suspicious_certificates = certificate_check(mismatched_resolutions)
+	suspicious_certificates = certificate_check(812)
 	print(f"\tList of suspicious certificates created -> ./certificates.csv")
 	print(f"\tObtaining web pages of suspicious websites...")
 	curler(suspicious_certificates)
